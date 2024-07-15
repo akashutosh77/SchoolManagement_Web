745d23bf7dd1f78654280b3b06b1336d4a7ad008
Ashutosh Kumar <akashutosh77@gmail.com>
2024-07-15 14:51:02 +0530

    Data as started coming from DB, 15-07-2024 14:50

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import { UploadButton } from "./components/UploadButton";
 import { ThemeProvider } from "@mui/material";
 import { muiTheme } from "./styles/muiTheme";
 import { Background } from "./components/Background";
 import { DrawerAppBar } from "./components/DrawerAppBar";
+import { fetchData } from "./services/masterService";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+      const getData = async () => {
+          try {
+              const result = await fetchData();
+              setData(result);
+              setLoading(false);
+          } catch (error) {
+              console.error('Error fetching data:', error);
+              setLoading(false);
+          }
+      };
+
+      getData();
+  }, []);
+
+  if (loading) {
+      return <div>Loading...</div>;
+  }else{
+    console.log('the data fetched is', data);
+  }
   return (
     <ThemeProvider theme={muiTheme}>
       <Background>
         <DrawerAppBar />
         <div style={{ marginTop: "100px" }}>
           {/* <UploadButton /> */}
+          
+
         </div>
       </Background>
     </ThemeProvider>
--- src/services/masterService.ts
+++ src/services/masterService.ts
@@ -1,1 +1,13 @@
-export {}
+import axios from "axios";
+
+const getMasterSchoolData_URL = "http://localhost:3000/schoolmanagement/getMasterSchoolData"; // Replace with your actual API endpoint
+
+export const fetchData = async () => {
+  try {
+    const response = await axios.get(getMasterSchoolData_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
